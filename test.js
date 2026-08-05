247092a01ab25a18ad7aa39df95d560b1ae1fbe6
“vijsanya27-afk” <vijaysanya27@gmail.com>
2026-08-05 11:10:06 +0530

    Added student registration form

--- test.js
+++ test.js
@@ -13,7 +13,21 @@ if(fs.existsSync("css/style.css")) {
         console.log("TC-02 :style.css exists:PASS");
      }
 else{
-             console.log("TC-02 : style.css exists:FAIL");
-             passed=false;
+    console.log("TC-02 : style.css exists:FAIL");
+    passed=false;
+}
+if(fs.existsSync("script.js")) {
+    console.log("TC-02 :script.jss exists:PASS");
+ }
+else{
+console.log("TC-02 : script.js exists:FAIL");
+passed=false;
+}
+if(fs.existsSync("data")) {
+    console.log("TC-02 :style.css exists:PASS");
+ }
+else{
+console.log("TC-02 : style.css exists:FAIL");
+passed=false;
 }
      
