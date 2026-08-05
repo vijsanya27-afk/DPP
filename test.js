e0ee36a5e19210763924931892d4bc5d5805bd18
vijsanya27-afk <vijaysanya27@gmail.com>
2026-08-05 10:57:31 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let passed = true;
+console.log("Registration Test\n")
+
+if(fs.existsSync("index.html")) {
+   console.log("TC-01 : index.html exists:PASS");
+}
+else{
+        console.log("TC-01 : index.html exists:FAIL");
+        passed=false;
+     }
+if(fs.existsSync("css/style.css")) {
+        console.log("TC-02 :style.css exists:PASS");
+     }
+else{
+             console.log("TC-02 : style.css exists:FAIL");
+             passed=false;
+}
+     
